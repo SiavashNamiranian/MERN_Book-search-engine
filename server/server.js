11da2c3abfb14a2b3fcf685401fde75f89debbe9
Accurast <siavash65n@gmail.com>
2023-09-04 23:40:16 -0700

    changes2

--- server/server.js
+++ server/server.js
@@ -8,8 +8,20 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
+
 const PORT = process.env.PORT || 3001 || 'mongodb://127.0.0.1:27017/MERN_Booksearchengine' ;
+
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+
+
+const startApolloServer = async () => {
+  await server.start();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,11 +33,17 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
-
 }
+
+
+
+app.use('/graphql', expressMiddleware(server));
 
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+};
+
+startApolloServer();
