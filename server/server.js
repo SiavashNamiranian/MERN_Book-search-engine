c71c60a853f47abaa28b38b066e45e860d161d25
Accurast <siavash65n@gmail.com>
2023-09-04 22:01:42 -0700

    changes

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
 const express = require('express');
+
+const { ApolloServer } = require('@apollo/server');//added//
+
+const { expressMiddleware } = require('@apollo/server/express4');//added//
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 'mongodb://127.0.0.1:27017/MERN_Booksearchengine' || 3001;
+const PORT = process.env.PORT || 3001 || 'mongodb://127.0.0.1:27017/MERN_Booksearchengine' ;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+
 }
 
 app.use(routes);
