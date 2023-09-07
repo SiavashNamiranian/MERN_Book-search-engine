5eb78eb60fcdcb0aa1808abccdcb7f2f774e6f2b
Accurast <siavash65n@gmail.com>
2023-09-06 19:50:28 -0700

    changes

--- server/server.js
+++ server/server.js
@@ -3,30 +3,38 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 
 const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('../server/utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
 
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 
 
 const server = new ApolloServer(
-// {
-  // typeDefs,
-  // resolvers,
-// }
+{
+  typeDefs,
+  resolvers,
+}
 );
 
-const app = express();
+
 
 const startApolloServer = async () => {
   await server.start();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/graphql', expressMiddleware(server, {
+  context: authMiddleware
+}));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
