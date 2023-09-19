9e87afdaf26410310106dd8a8464192b55c5d779
SiavashNamiranian <123912503+SiavashNamiranian@users.noreply.github.com>
2023-09-19 01:33:06 -0700

    changes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,36 +24,36 @@ export const ADD_USER = gql`
   }
 `;
 
-// export const SAVE_BOOK = gql`
-//   mutation saveBook($authors: String!, $description: String!, $bookId: String!, $image: String!, $link: String!, $title: String!) {
-//     saveBook (authors: $authors!, description: $description!, bookId: $bookId!, image: $image!, link: $link!, title: $title!) {
-//       authors
-//       description
-//       bookId
-//       image
-//       link
-//       title
-//     }
-//   }
-// `;
 export const SAVE_BOOK = gql`
-  mutation saveBook($authors: [String]!, $description: String!, $bookId: String!, $image: String!, $link: String!, $title: String!) {
-    saveBook(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
-      _id
-      username
-      email
-      bookCount
-      savedBooks {
-        authors
-        description
-        bookId
-        image
-        link
-        title
-      }
+  mutation saveBook($authors: String!, $description: String!, $bookId: String!, $image: String!, $link: String!, $title: String!) {
+    saveBook (authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+      authors
+      description
+      bookId
+      image
+      link
+      title
     }
   }
 `;
+// export const SAVE_BOOK = gql`
+//   mutation saveBook($authors: [String]!, $description: String!, $bookId: String!, $image: String!, $link: String!, $title: String!) {
+//     saveBook(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+//       _id
+//       username
+//       email
+//       bookCount
+//       savedBooks {
+//         authors
+//         description
+//         bookId
+//         image
+//         link
+//         title
+//       }
+//     }
+//   }
+// `;
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
