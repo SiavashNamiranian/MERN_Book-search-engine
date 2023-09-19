f0ab24ae6ef02d761f6e3ed1d4fab0113d0bc667
SiavashNamiranian <123912503+SiavashNamiranian@users.noreply.github.com>
2023-09-18 18:12:38 -0700

    changes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,33 +1,34 @@
 const typeDefs = `
-type User {
-  _id: ID
-  username: String
-  email: String
-  password: String
-  savedBooks: [String]!
-}
-type Book {
-  _id: ID
-  authors: [String]
-  description: String
-  bookId: String
-  image: String
-  link: String
-  title: String
-}
-type Auth {
-  token: ID!
-  user: User
-}
-type Query {
-  me: User
-}
-type Mutation {
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [String]!
+  }
+  type Book {
+    _id: ID
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
   addUser(username: String!, email: String!, password: String!): Auth
   login(email: String!, password: String!): Auth
   saveBook(authors: String!, description: String!, bookId: String!, image: String!, link: String!): User
   deletBook(bookId: String!): User
-}
+  }
+
 `;
 
 module.exports = typeDefs;
